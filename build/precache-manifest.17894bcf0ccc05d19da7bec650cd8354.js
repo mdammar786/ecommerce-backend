42d172ba971132958912a76f360f6a05290ac22e
mdammar786 <98681797+mdammar786@users.noreply.github.com>
2022-08-22 19:15:53 +0530

    Add files via upload

---
+++ build/precache-manifest.17894bcf0ccc05d19da7bec650cd8354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74181c29f7617d90cbb25de2602792e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ca4dd29c0a881baa41",
+    "url": "/static/js/2.ba93ba03.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.ba93ba03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b42a3f95bf1e68a529",
+    "url": "/static/js/main.1fa3bf87.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "3bf24e4b8d8a2563872313e085751b5f",
+    "url": "/static/media/commerce.3bf24e4b.png"
+  }
+]);
